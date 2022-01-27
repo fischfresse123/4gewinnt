0cdff5f6d8a96ecdc32692f578c9e734a57e3626
fischfresse123 <svnkahle@gmail.com>
2022-01-27 20:07:12 +0100

    spielfeld unfertig 1

--- 4gewinnt/src/hallo/haupt.java
+++ 4gewinnt/src/hallo/haupt.java
@@ -3,9 +3,8 @@ package hallo;
 public class haupt {
 
 	public static void main(String[] args) {
-		System.out.println("hallo");
-		System.out.println("123");
-		//hallo
+		System.out.println("|   |   |   |   |   |   |\n————————————————————————");
+		
 		
 	}
 
