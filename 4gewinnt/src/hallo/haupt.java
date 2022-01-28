5f649b4600156eee6e6980c9a310e008a32982e7
Max310 <Max310@ws305-214.schule.bremen.de>
2022-01-28 08:35:10 +0100

    feld 1.0

--- 4gewinnt/src/hallo/haupt.java
+++ 4gewinnt/src/hallo/haupt.java
@@ -1,12 +1,63 @@
 package hallo;
 
+
+
+import javax.swing.JFrame;
+
+import javax.swing.JLabel;
+
+
+
 public class haupt {
 
+
+
 	public static void main(String[] args) {
-		System.out.println("|   |   |   |   |   |   |\n————————————————————————");
-		System.out.println("test");
-		
-		
+
+		System.out.println("Hallo die zweite");
+
+		System.out.println("hallo");
+
+		System.out.println(
+
+				"  1   2   3   4   5   6   7\n  v   v   v   v   v   v   v\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|");
+
+		JFrame fenster = new JFrame("4Gewinnt");
+
+		fenster.setSize(600, 400);
+
+		fenster.setVisible(true);
+
+		fenster.add(new JLabel(
+
+				"  1   2   3   4   5   6   7\n  v   v   v   v   v   v   v\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|\n|   |   |   |   |   |   |   |\n|———————————————————————————|"));
+
+		fenster.add(new JLabel("  v   v   v   v   v   v   v"));
+
 	}
+
+
+
+	public static boolean abfrage(int z) {
+
+		for (int x = 0; x < 7; x++) {
+
+			for (int y = 0; y < 6; y++) {
+
+
+
+			}
+
+		}
+
+		boolean hallo = true;
+
+		return hallo;
+
+		
+
+	}
+
+
 
 }
