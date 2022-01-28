672def7cc37be9f079da4f2479ebddc0ec345c2a
Max310 <Max310@ws305-214.schule.bremen.de>
2022-01-28 08:30:02 +0100

    dsdsds

--- 4gewinnt/src/hallo/haupt.java
+++ 4gewinnt/src/hallo/haupt.java
@@ -4,6 +4,7 @@ public class haupt {
 
 	public static void main(String[] args) {
 		System.out.println("|   |   |   |   |   |   |\n————————————————————————");
+		System.out.println("test");
 		
 		
 	}
