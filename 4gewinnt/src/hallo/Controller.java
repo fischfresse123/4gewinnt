439eec235f47a3c8f61357ce06513321078a7e94
fischfresse123 <svnkahle@gmail.com>
2022-02-02 17:40:28 +0100

    Blaue Buttons

--- 4gewinnt/src/hallo/Controller.java
+++ 4gewinnt/src/hallo/Controller.java
@@ -12,7 +12,7 @@ public class Controller implements ActionListener {
 		if (command.equals("Start Game")) {
 			System.out.println("Spiel startet...");
 		} else if (command.equals("Regeln")) {
-			JOptionPane.showMessageDialog(null,"Jeder Spieler besitzt 21 gleichfarbige Spielsteine. Wenn ein Spieler einen Spielstein in eine Spalte fallen lässt, \n besetzt dieser den untersten freien Platz der Spalte. \n Gewinner ist der Spieler, der es als erster schafft, vier oder mehr seiner \n Spielsteine waagerecht, senkrecht oder diagonal in eine Linie zu bringen. Das Spiel endet unentschieden, \n wenn das Spielbrett komplett gefüllt ist, ohne dass ein Spieler eine Viererlinie gebildet hat.");
+			JOptionPane.showMessageDialog(null,"Jeder Spieler besitzt 21 gleichfarbige Spielsteine. Wenn ein Spieler einen Spielstein in eine Spalte fallen lässt, \n besetzt dieser den untersten freien Platz der Spalte. \n Gewinner ist der Spieler, der es als erster schafft, vier oder mehr seiner \n Spielsteine waagerecht, senkrecht oder diagonal in eine Linie zu bringen. Das Spiel endet unentschieden, \n wenn das Spielbrett komplett gefüllt ist, ohne dass ein Spieler eine Viererlinie gebildet hat.", "Regeln", JOptionPane.INFORMATION_MESSAGE);
 			System.out.println("Die Regeln werden angezeigt");
 		} else if (command.equals("Exit Game")) {
 			System.exit(0);
--- 4gewinnt/src/hallo/Fenster.java
+++ 4gewinnt/src/hallo/Fenster.java
@@ -13,9 +13,22 @@ public class Fenster extends JFrame {
 		add(menu);
 
 		JButton start = new JButton("Start Game");
+		start.setBackground(Color.BLUE);
+		start.setForeground(Color.WHITE);
+		start.setFocusPainted(false);
+		start.setFont(new Font("Tahoma", Font.BOLD, 12));
 		JButton regeln = new JButton("Regeln");
+		regeln.setBackground(Color.BLUE);
+		regeln.setForeground(Color.WHITE);
+		regeln.setFocusPainted(false);
+		regeln.setFont(new Font("Tahoma", Font.BOLD, 12));
 		JButton exit = new JButton("Exit Game");
+		exit.setBackground(Color.BLUE);
+		exit.setForeground(Color.WHITE);
+		exit.setFocusPainted(false);
+		exit.setFont(new Font("Tahoma", Font.BOLD, 12));
 		JLabel label = new JLabel();
+		
 		label.setText("---");
 
 		menu.add(start);
